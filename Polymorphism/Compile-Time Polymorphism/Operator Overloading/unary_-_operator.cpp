841765ac33166e0e39a40487282b96cdbe4611aa
SurajSah776 <surajkumarsah2060@gmail.com>
2024-09-23 20:30:37 +0530

    Unary Minus(-) Operator

---
+++ Polymorphism/Compile-Time Polymorphism/Operator Overloading/unary_-_operator.cpp
@@ -0,0 +1,46 @@
+/*
+ *** Overloading Unary minus (-) Operator
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Class Structure
+
+class UnaryMinus
+{
+
+private:
+    int a;
+
+public:
+    // Constructor
+    UnaryMinus(int a) : a(a) {}
+
+    // Overloading unary minus (-)
+    void operator-()
+    {
+        a = -a;
+    }
+
+    // Function to display the result
+    void display()
+    {
+        cout << "a = " << a << endl;
+    }
+};
+
+// Driver Code
+int main()
+{
+    UnaryMinus obj(6);
+
+    obj.display();
+
+    // Unary Minus(-)
+    -obj;
+    cout << "After -a, ";
+    obj.display();
+
+    return 0;
+}
